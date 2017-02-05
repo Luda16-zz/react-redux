a8b3ef70b508ca545615628a6657a273ac6b7605
Luda Noko <ldcg1@hotmail.com>
2017-02-05 12:24:32 -0400

    Create store for Redux setting

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -10,15 +10,15 @@ import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
 
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-const router = {
+const router = (
 	<Router history={browserHistory}>
 		<Route path="/" component={Main}>
 			<IndexRoute component={PhotoGrid}></IndexRoute>
 			<Route path="/view/:postID" component={Single}></Route>
 	    </Route>
 	</Router>
-}
+)
 
 render(router, document.getElementById('root'));
